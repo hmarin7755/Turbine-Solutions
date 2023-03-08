51d261655c788b85437040cf63a1e520fdacdd17
Humberto Marin <hmarin2698@gmail.com>
2023-03-07 23:30:32 -0500

    add bootstrap

--- script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', ()=>{
-    let nav = document.querySelector('nav');
-    nav.classList.toggle('sticky', window.scrollY > 0 );
-});
-
-
-let changeIcon = function(icon){
-    icon.classList.toggle('fa-xmark')
-}
