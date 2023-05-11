80ef2a9ddbd64b2c09e65a1995208a7b0b1365d7
Humberto Marin <hmarin2698@gmail.com>
2023-05-10 20:21:46 -0400

    navbar collapses on mobile when link is clicked

--- custom.js
+++ custom.js
@@ -1,4 +1,18 @@
-const navEl = document.querySelector('.navbar');
-window.addEventListener('scroll', ()=>{
-    navEl.classList.toggle('navbar-scrolled', window.scrollY > 65 );
+//displays background color on navbar when scrolled
+const navEl = document.querySelector(".navbar");
+window.addEventListener("scroll", () => {
+  navEl.classList.toggle("navbar-scrolled", window.scrollY > 65);
+});
+
+//collapses nav bar on mobile when link is clicked
+const navLinks = document.querySelectorAll(".nav-item:not(.dropdown)");
+const menuToggle = document.getElementById("main-nav");
+const bsCollapse = new bootstrap.Collapse(menuToggle, { toggle: false });
+navLinks.forEach(function (l) {
+  l.addEventListener("click", function () {
+    // avoid flickering on desktop
+    if (menuToggle.classList.contains("show")) {
+      bsCollapse.toggle();
+    }
+  });
 });
